a1c5382b5074dfb41c70586496aeac019fcbabbc
Cory Stephenson <34756469+coryStephenson@users.noreply.github.com>
2022-06-03 17:45:47 -0500

    1st draft of script "preamble"
    I have written the initial error handling statements and wrote a brief overview of the tasks to be carried out by the script.

---
+++ disk-management.sh
@@ -0,0 +1,23 @@
+#! /usr/bin/env bash
+set -euo pipefail
+set -o errtrace
+
+echo -e "\n\n*******************************************************************************************************\n
+Name of Script: $0\n
+Author: Cory Stephenson\n
+View script for further description of tasks\n
+Tasks:\n
+1) Partition disk\n
+2) Format disk\n
+3) Run diagnostics on disk\n
+4) Pad disk with data stream\n
+5) Erase disk\n
+6) Backup disk\n
+7) Clone disk\n
+8) Restore disk from backup\n
+9) Display disk's free space info\n
+10) Display disk's used space info\n
+*******************************************************************************************************************\n\n"
+
+
+
