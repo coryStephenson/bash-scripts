1ca3f22a010c9f4141fddb1b14b595c7d74009cd
Cory Stephenson <34756469+coryStephenson@users.noreply.github.com>
2025-10-20 14:38:37 -0500

    Added commands for installing Wezterm

--- installation-scripts/application-installations.sh
+++ installation-scripts/application-installations.sh
@@ -68,3 +68,16 @@ while read -r p ; do sudo apt-get install -y $p ; done < <(cat << "EOF"
 EOF
 )
 
+# Installing Wezterm (check website for any command updates)
+flatpak install flathub org.wezfurlong.wezterm
+curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /usr/share/keyrings/wezterm-fury.gpg
+curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /usr/share/keyrings/wezterm-fury.gpg
+echo 'deb [signed-by=/usr/share/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list
+sudo chmod 644 /usr/share/keyrings/wezterm-fury.gpg
+sudo apt update
+sudo apt install wezterm
+cd ..
+mkdir -p .config/wezterm
+touch .config/wezterm/wezterm.lua
+cd .config/wezterm
+kate wezterm.lua
